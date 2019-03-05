e541a08cd7c4952c9258e31cd8e06abbb80b6f19
mexi-taco <43611348+mexi-taco@users.noreply.github.com>
2019-03-05 09:32:36 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#pics").click(function(){
+ $(".magicians") .fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#image").click(function(){
+ $(".wand") .slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#cards").click(function(){
+ $(".deck") .fadeToggle();
+});
+
+
+
